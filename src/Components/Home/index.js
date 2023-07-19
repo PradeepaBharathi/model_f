8e24beb004a0354e0e05c4bbecb095aff476427e
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-07-19 20:10:43 +0530

    home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,10 +4,12 @@ import LogButton from '../LogButton';
 function Home() {
   return (
     <>
+    <div className='d-flex flex-column justify-content-center align-items-center'>
     <h5 className='m-5 text-break'>Welcome to URL shortener App</h5>
     {(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Hi {localStorage.getItem("loggedUsername")} !</h6>}
     {!(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Log in to use the app.</h6>} 
     <LogButton/>
+    </div>
     </>
   )
 }
