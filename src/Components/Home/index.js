81edf1309e6ab463c41739a98aef20e2e6c2f57a
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-07-19 20:03:46 +0530

    button

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LogButton from '../LogButton';
 
 function Home() {
   return (
@@ -6,6 +7,7 @@ function Home() {
     <h5 className='m-5 text-break'>Welcome to URL shortener App</h5>
     {(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Hi {localStorage.getItem("loggedUsername")} !</h6>}
     {!(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Log in to use the app.</h6>} 
+    <LogButton/>
     </>
   )
 }
