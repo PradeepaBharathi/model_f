813e70a9dd055bff10357016443dd00785cad5e8
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-07-19 19:21:44 +0530

    edit

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,8 +4,7 @@ function Home() {
   return (
     <>
     <h5 className='m-5 text-break'>Welcome to URL shortener App</h5>
-    {(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Hi {localStorage.getItem("loggedUsername")} !63+
-    </h6>}
+    {(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Hi {localStorage.getItem("loggedUsername")} !</h6>}
     {!(localStorage.getItem("loggedUsername")) && <h6 className='my-2'>Log in to use the app.</h6>}
     </>
   )
