0711e0630064d0ab6a898e3057aeff840354ed8c
KalaivaniBaskar <kalaivani.working@gmail.com>
2023-07-19 16:43:02 +0530

    netlify deploy1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Login from './Components/Login';
+import Sent from './Components/Sent';
+import Signup from './Components/Signup';
+import ForgotPwd from './Components/ForgotPwd';
+import DashboardURL from './Components/DashboardURL';
+import CreateURL from './Components/CreateURL';
+import AllURL from './Components/AllURL';
+import Header from './Components/Header';
+import Authorize from './Components/Authorize';
+import ActivateAccount from './Components/ActivateAccount';
+import { NameProvider } from './Context/context';
+import ActivationMail from './Components/ActivationMail/index.js';
+import ResetPwd from './Components/ResetPwd';
+import Redirect from './Components/Redirect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NameProvider> 
+        <Header/>
+        <Routes>
+        <Route path="/" element={ <Login/>} ></Route>
+        <Route path="/login" element={ <Login/>} ></Route>
+        <Route path="/signup" element={ <Signup/>} ></Route>
+        <Route path="/forgot-password" element={ <ForgotPwd/>} ></Route>
+        <Route path="/activation" element={ <ActivationMail/>} ></Route>
+        <Route path="/sent" element={ <Sent/>} ></Route>
+        <Route path="/reset/:id" element={ <ResetPwd/>} ></Route>
+        <Route path="/activate/:id" element={ <ActivateAccount/>} ></Route>
+        <Route path="/authorize" element={ <Authorize/>} ></Route>
+        <Route path="/dashboard-url" element={ <DashboardURL/>} ></Route>
+        <Route path="/create-url" element={ <CreateURL/>} ></Route>
+        <Route path="/all-url" element={ <AllURL/>} ></Route>
+        <Route path="/:id" element={ <Redirect/>} ></Route>
+        </Routes>
+      </NameProvider>
     </div>
   );
 }
---
+++ src/Components/Redirect/index.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios"
+import { useGlobalContext } from '../../Context/context';
+
+function Redirect() {
+    const {id} = useParams(); 
+    const {serverURL} = useGlobalContext();
+    const getData = async(id) => {
+    const response = await axios.get(`${serverURL}/${id}`)
+    //console.log(response);
+    window.location.href = response.data.longURL;
+  }
+  useEffect(  ()=> {
+    getData(id);
+  },[])
+  return (
+    <div className='m-5'>You are being redirected...</div>
+  )
+}
+
+export default Redirect;
